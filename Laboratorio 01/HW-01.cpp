781be10f0df219c72ddfa424fd78e525d0e5f614
Luismeza1 <00311319@uca.edu.sv>
2020-01-13 18:43:49 -0600

    finished HW-01

--- Laboratorio 01/HW-01.cpp
+++ Laboratorio 01/HW-01.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
-    return 0;
-    
+void suma(int *p){
+  int sumaPrimos = 0;
+  for(int i = 0; i < 25; i++){
+    sumaPrimos += *(p+i);
+  }
+
+  cout << "La suma de los números primos de 1 a 100 es: "<<sumaPrimos<<endl;
 }
+
+int main(){
+    int n=100, c=0, c2=0, res=0, nc=0, primos=0;
+    int sp[25];
+  
+  for(c=1;c<=n;c++)
+  {
+    for(c2=1;c2<=c;c2++)
+    {
+      res=c%c2;
+      if(res==0)
+      {
+        nc=nc+1;
+      }
+    }
+    if(nc==2)
+    {
+      sp[primos] = c;
+      primos++;
+    }
+    nc=0;
+  }
+
+  suma(sp);
+
+  return 0;
+
+}
+
