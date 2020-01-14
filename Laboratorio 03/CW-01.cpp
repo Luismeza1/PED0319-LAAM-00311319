41fdfd34422a338fe0bbad0eb5fd54c7288bc97b
Luismeza1 <00311319@uca.edu.sv>
2020-01-14 12:46:57 -0600

    Add: recursivo

---
+++ Laboratorio 03/CW-01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+long int factorial(int n);
+
+int main(void)
+{
+    cout << factorial(10) << endl;
+
+    return 0;
+}
+
+long int factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
