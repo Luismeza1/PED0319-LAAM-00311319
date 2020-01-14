ad1ba2195d170c35d43eb36fad4873e8951b3b6a
Luismeza1 <00311319@uca.edu.sv>
2020-01-13 22:17:35 -0600

    Added comments and HW-03

--- Laboratorio 01/HW-01.cpp
+++ Laboratorio 01/HW-01.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void suma(int *p){
+void suma(int *p){//Iniciamos la función suma
   int sumaPrimos = 0;
-  for(int i = 0; i < 25; i++){
-    sumaPrimos += *(p+i);
+  for(int i = 0; i < 25; i++){//Declaramos el bucle for, con i de 0 a 24
+     sumaPrimos += *(p+i);//Agregamos el numero primo con el contador a la posición i del arreglo 
   }
 
-  cout << "La suma de los números primos de 1 a 100 es: "<<sumaPrimos<<endl;
+  cout << "La suma de los números primos de 1 a 100 es: "<<sumaPrimos<<endl;//Imprimimos la suma
 }
 
-int main(){
-    int n=100, c=0, c2=0, res=0, nc=0, primos=0;
-    int sp[25];
+int main(void){
+    int n=100, c=0, c2=0, res=0, nc=0, primos=0; //Declaramos las variables y las inicializamos a 0, con excepción de n que inicializamos a 100
+    int sp[25]; //Declaramos el arreglo, tamaño 25, que es el numero de primos que hay de 1 a 100
   
-  for(c=1;c<=n;c++)
+  for(c=1;c<=n;c++)//Declaramos el bucle for sin declarar la variable ahi para que no se destruya, vamos a utilizar su valor
   {
-    for(c2=1;c2<=c;c2++)
+    for(c2=1;c2<=c;c2++)//Declaramos el segundo bucle for, haciendo lo mismo que con el primero
     {
       res=c%c2;
       if(res==0)
---
+++ Laboratorio 01/HW-03.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isNumber(string word){//Funcion booleana para ver si es numero
+    bool hasNumber = true;//No sé usar bandera, busqué en varios codigos y vi como funcionaba al igual que el metodo .size
+    int tam;
+    tam = word.size();
+    for(int i = 0; i < tam; i++){
+        if(word[i] <=48 || word[i] >=58 ){
+            hasNumber = false;            
+            break;
+        }
+    }
+    return hasNumber;
+}
+
+int main(void){
+string n;
+cout << "Digite cadena de caracteres: " << endl;
+cin >> n;
+
+while(isNumber(n)){
+    cout << "Valor de cadena de caracteres: 1" <<endl;//si es cadena de numeros imprime 1 y uso break para que no sea una impresion infinita
+    break;
+}
+
+while(!isNumber(n)){
+    cout << "Valor de cadena de caracteres: -1" <<endl;//Si es cadena de letras imprime -1 y lo mismo con el break.
+    break;
+}
+
+return 0;
+}
+
