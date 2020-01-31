2de7457dbd2dbccc25b88ce199487b840d2fad82
Luismeza1 <00311319@uca.edu.sv>
2020-01-30 19:36:51 -0600

    Mod: CW-02

--- Laboratorio 07/CW-02.cpp
+++ Laboratorio 07/CW-02.cpp
@@ -28,20 +28,20 @@ int main(void){
 
         switch(opcion){
             case 1:
-                cout << "Titulo:\t"; getline(cin, aux.titulo);
-                cout << "cantante:\t"; getline(cin, aux.cantante);
-                cout << "genero: 1-a, 2-b, 3-c ,4-d:\t"; cin >> auxEnum; cin.ignore();
+                cout << "Titulo: "; getline(cin, aux.titulo);
+                cout << "cantante: "; getline(cin, aux.cantante);
+                cout << "genero: 1-a, 2-b, 3-c ,4-d: ";
                 cin >> auxEnum; cin.ignore(); auxEnum--;
                 aux.gen = genero(auxEnum);
                 aux.duracion = 0;
                 do{
-                    cout << "Duracion:\t"; cin >> aux.duracion; cin.ignore();
+                    cout << "Duracion: "; cin >> aux.duracion; cin.ignore();
                 }while(aux.duracion <= 0);
                 lista.push_back(aux);
             break;
             case 2:
-                cout << "Cancion a eliminar:\t"; getline(cin, tit);
-                for(auto iter = lista.begin(); iter != lista.end; ++iter){
+                cout << "Cancion a eliminar: "; getline(cin, tit);
+                for(auto iter = lista.begin(); iter != lista.end(); ++iter){
                     if(iter->titulo.compare(tit))
                         iter = lista.erase(iter);
                     else
@@ -52,7 +52,7 @@ int main(void){
 
             case 3:
                 for(T elemento : lista){
-                    cout << elemento.cantante << endl;
+                    cout << "\n" << elemento.cantante << endl;
                     cout << elemento.titulo << endl;
                     cout << elemento.duracion << endl;
                     switch(elemento.gen){
@@ -93,12 +93,12 @@ int main(void){
 }
 
 void imprimirMenu(void){
-    cout << "1.\tAgregar cancion" << endl;
+    cout << "\n1.\tAgregar cancion" << endl;
     cout << "2.\tEliminar cancion" << endl;
     cout << "3.\tMostrar canciones" << endl;
     cout << "4.\tBuscar cancion" << endl;
     cout << "0.\tSalir" << endl;
-    cout << "Su opcion: " << endl;
+    cout << "Su opcion:\t";
 }
 
 void buscarElemento(vector<T> canciones, queue<T>* cola, string tit){
@@ -110,5 +110,5 @@ void buscarElemento(vector<T> canciones, queue<T>* cola, string tit){
         }
             
     }
-    cout << "Cancion" << endl;
+    cout << "Cancion agregada" << endl;
 }
